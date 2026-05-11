be4a35c66ca152de5064028f355472424774b97c
yesimyigitbasi <120514583+yesimyigitbasi@users.noreply.github.com>
2026-05-11 09:46:18 +0300

    fix

--- student_hub/ios/Runner/AppDelegate.swift
+++ student_hub/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyCrqD9DH9tAD6GIrr41w9kaiUGm9IqEhNU")
+    GMSServices.provideAPIKey([GOOGLE_MAPS_API_KEY])
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
